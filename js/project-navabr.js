d6aa1e0701a1990aedb3ab03706835ea57354bfe
jaydhas238-beep <jaydhas238@gmail.com>
2026-07-24 14:22:07 +0530

    Update project-navabr.js

--- js/project-navabr.js
+++ js/project-navabr.js
@@ -1,52 +1,7 @@
 console.log("Navbar JS Loaded");
 
-const projectsBtn = document.getElementById("projectsBtn");
-const projectsMenu = document.getElementById("projectsMenu");
-const projectTypes = document.querySelectorAll(".project-type");
-const projectLists = document.querySelectorAll(".project-list");
+const btn = document.getElementById("projectsBtn");
 
-projectsBtn.onclick = function(e){
-
-    e.stopPropagation();
-
-    projectsMenu.classList.toggle("show");
-
-    projectLists.forEach(list=>{
-
-        list.classList.remove("show");
-
-    });
-
+btn.onclick = function () {
+    alert("Button Working");
 };
-
-projectTypes.forEach(type=>{
-
-    type.onclick=function(e){
-
-        e.stopPropagation();
-
-        projectLists.forEach(list=>{
-
-            list.classList.remove("show");
-
-        });
-
-        document
-            .getElementById(type.dataset.target)
-            .classList.add("show");
-
-    }
-
-});
-
-document.onclick=function(){
-
-    projectsMenu.classList.remove("show");
-
-    projectLists.forEach(list=>{
-
-        list.classList.remove("show");
-
-    });
-
-}
